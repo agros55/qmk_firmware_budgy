6525a2588a25bd3b474958abbd0cadbb0a710db2
Fernando Octavio Viveros <39587296+agros55@users.noreply.github.com>
2025-01-31 17:31:17 -0300

    Update keymap.c

--- keyboards/budgy/keymaps/default/keymap.c
+++ keyboards/budgy/keymaps/default/keymap.c
@@ -47,15 +47,19 @@ enum layer {
 #define CIRC ALGR(LCBR) // ^
 #define GRV  ALGR(KC_NUHS) // `
 
+// Layers buttons
+#define NUM MO(1)
+#define SYM MO(2)
+#define MOD MO(3)
 // Tap Dance declarations
 enum {
-    TD_NTIL,
+    TD_N,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
 // Tap once for Escape, twice for Caps Lock
-      [TD_NTIL] = ACTION_TAP_DANCE_DOUBLE(KC_N, NTIL),
+      [TD_N] = ACTION_TAP_DANCE_DOUBLE(KC_N, NTIL),
 };
 
 
@@ -69,35 +73,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │  -  │  Q  │  J  │  K  │  X  │       │  B  │  M  │  '  │  V  │  Z  │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *                        ┌───┐                 ┌───┐
-      *                        │ Bsp ├───┐   ┌───┤ Ent │
-      *                        └───┤ Tab │   │ Spc ├───┘
-      *                               └───┘   └───┘
+      *                ┌───┐                              ┌───┐
+      *                │ Bsp ├───────┐   ┌───────┤ Ent │
+      *                └───┤ Tab │ NUM |    | SYM  │ Spc ├───┘
+      *                       └───────┘   └───────┘
       */
     [DVORAK] = LAYOUT_split_3x5_2(
-        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_H,    KC_L,
-        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_R,    KC_T,    TD(TD_NTIL),    KC_S,
-        GUION,   KC_Q,    KC_J,    KC_K,    KC_X,                               KC_B,    KC_M,    ACENT,   KC_V,    KC_Z,
-                                            KC_BSPC, KC_TAB,           KC_SPC,  KC_ENT
+        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_H,     KC_L,
+        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_R,    KC_T,    TD(TD_N), KC_S,
+        GUION,   KC_Q,    KC_J,    KC_K,    KC_X,                               KC_B,    KC_M,    ACENT,   KC_V,     KC_Z,
+                                   KC_BSPC, KC_TAB, NUM,                   SYM, KC_SPC,  KC_ENT
     ),
     /*
       * NUMBER
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │  .  │  ,  │  W  │  P  │  Y  │       │  F  │  7  │  8  │  9 │  L  │
+      * │  F9 │ F10 │ F11 │ F12 │  Y  │       │  *  │  7  │  8  │  9  │  /  │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │  A  │  O  │  E  │  U  │  I  │       │  D  │  4  │  5 │ N/Ñ │  S  │
+      * │  F5 │  F6 │  F7 │  F8 │  .  │       │  +  │  4  │  5  │  6  │  -  │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │  -  │  Q  │  J  │  K  │  X  │       │  B  │  M  │  '  │  V  │  Z  │
+      * │  F1 │  F2 │  F3 │  F4 │  X  │       │  0  │  1  │  2  │  3  │  .  │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *                        ┌───┐                 ┌───┐
-      *                        │ Bsp ├───┐   ┌───┤ Ent │
-      *                        └───┤ Tab │   │ Spc ├───┘
-      *                               └───┘   └───┘
+      *                ┌───┐                              ┌───┐
+      *                │ Bsp ├───────┐   ┌───────┤ Ent │
+      *                └───┤ Tab │ NUM |    | SYM  │ Spc ├───┘
+      *                       └───────┘   └───────┘
       */
     [NUMBER] = LAYOUT_split_3x5_2(
-        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_H,    KC_L,
-        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_R,    KC_T,    TD(TD_NTIL),    KC_S,
-        GUION,   KC_Q,    KC_J,    KC_K,    KC_X,                               KC_B,    KC_M,    ACENT,   KC_V,    KC_Z,
-                                            KC_BSPC, KC_TAB,           KC_SPC,  KC_ENT
+        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_7,    KC_8,    KC_9,    KC_L,
+        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_4,    KC_5,    KC_6,    KC_S,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_X,                               KC_0,    KC_1,    KC_2,    KC_3,    KC_Z,
+                                   KC_BSPC, KC_TAB, NUM,                   SYM, KC_SPC,  KC_ENT
     )
 };

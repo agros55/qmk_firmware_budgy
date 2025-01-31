da3823d4b790932e0498a402743c3f4e51ed3a85
Fernando Octavio Viveros <39587296+agros55@users.noreply.github.com>
2025-01-31 17:08:44 -0300

    1ra edición

--- keyboards/budgy/keymaps/default/keymap.c
+++ keyboards/budgy/keymaps/default/keymap.c
@@ -2,24 +2,31 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
+enum layer {
+DVORAK,
+SYMBOL,
+NUMBER,
+MOD,
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * │  .  │  ,  │  W  │  P  │  Y  │       │  F  │  G  │  C  │  H  │  L  │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * │  A  │  O  │  E  │  U  │  I  │       │  D  │  R  │  T  │ N/Ñ │  S  │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * │  -  │  Q  │  J  │  K  │  X  │       │  B  │  M  │  '  │  V  │  Z  │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *               ┌───┐           ┌───┐
-      *               │Bsp├───┐   ┌───┤Ent│
-      *               └───┤Tab│   │Spc├───┘
-      *                   └───┘   └───┘
+      *                        ┌───┐                ┌───┐
+      *                        │ Bsp ├───┐   ┌───┤ Ent │
+      *                        └───┤ Tab │   │ Spc ├───┘
+      *                               └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x5_2(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+    [DVORAK] = LAYOUT_split_3x5_2(
+        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_H,    KC_L,
+        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_R,    KC_T,    NTIL,    KC_S,
+        KC_Z,    KC_Q,    KC_J,    KC_K,    KC_X,                               KC_B,    KC_M,    ACENT,   KC_V,    KC_Z,
                                             KC_BSPC, KC_TAB,           KC_SPC,  KC_ENT
     )
 };

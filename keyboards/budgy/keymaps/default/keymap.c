7bd4bb7ebc68072e1558f0956f1637718dabb0d6
Fernando Octavio Viveros <39587296+agros55@users.noreply.github.com>
2025-02-01 12:21:58 -0300

    Update keymap.c F1-12

--- keyboards/budgy/keymaps/default/keymap.c
+++ keyboards/budgy/keymaps/default/keymap.c
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───────┘   └───────┘
       */
     [NUMBER] = LAYOUT_split_3x5_2(
-        KC_DOT,  KC_COMM, KC_W,    KC_P,    KC_Y,                               KC_F,    KC_7,    KC_8,    KC_9,    KC_L,
-        KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                               KC_D,    KC_4,    KC_5,    KC_6,    KC_S,
+        KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_Y,                               KC_F,    KC_7,    KC_8,    KC_9,    KC_L,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_I,                               KC_D,    KC_4,    KC_5,    KC_6,    KC_S,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_X,                               KC_0,    KC_1,    KC_2,    KC_3,    KC_Z,
                                    KC_BSPC, KC_TAB, NUM,                   SYM, KC_SPC,  KC_ENT
     )
